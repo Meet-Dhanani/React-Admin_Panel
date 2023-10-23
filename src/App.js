b8ac82551a411312e5180e0ea554d2473b48f710
Meet-Dhanani <meetdhanani8383@gmail.com>
2023-10-23 14:23:21 +0530

    React-Admin_Panel

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './component/Header';
+import Menu from './component/Menu';
+import Dashboard from './component/Dashboard';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Dashboard />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+
+<div>
+  <footer className="main-footer">
+    <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div className="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+  {/* Control Sidebar */}
+  <aside className="control-sidebar control-sidebar-dark">
+    {/* Control sidebar content goes here */}
+  </aside>
+  {/* /.control-sidebar */}
+</div>
+
+
+    </div>
+  )
+}
+
+export default Footer
